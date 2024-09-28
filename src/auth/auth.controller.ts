8e1217a05cc9cd562f0a52045e6163dd023ed02d
Lucila <lucilalang07@hotmail.com>
2024-09-28 17:18:32 -0300

    feat(auth): implement JWT authentication

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { JwtAuthGuard } from "./jwt-auth.guard";
+
+interface LoginUserDto {
+    username: string;
+    password: string;
+}
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService) {
+    }
+
+    @Post('login')
+    async login(@Body() loginUserDto: LoginUserDto) {
+        return this.authService.login(loginUserDto);
+    }
+}
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { jwtConstants } from './constants';
+import { AuthController } from "./auth.controller";
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60m' }, // Token expires in 60 minutes
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
